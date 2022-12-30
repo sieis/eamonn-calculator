faef1ce5535b13b755baf89cb7e197b800636563
Eamonn Cottrell <eamonncottrell@gmail.com>
2022-12-30 07:15:21 -0500

    some final styling and links to other projects.

--- index.js
+++ index.js
@@ -114,26 +114,26 @@ class App extends React.Component{
         return(
             <>
                 <div id="calculator">
-                    <div className="display" id="expression">{this.state.expression}</div>
-                    <div className="display" id="display">{this.state.display}</div>
+                    <div className="display display-top" id="expression">{this.state.expression}</div>
+                    <div className="display display-bottom" id="display">{this.state.display}</div>
                     <div id="operators">
-                        <button onClick={this.handleClear} id="clear" value="AC">AC</button>
-                        <button onClick={this.handleOperator} id="divide" value="/">/</button>
-                        <button onClick={this.handleOperator} id="multiply" value="x">*</button>
-                        <button onClick={this.handleNumber} type="num" id="nine" value="9">9</button>
-                        <button onClick={this.handleNumber} type="num" id="eight" value="8">8</button>
-                        <button onClick={this.handleNumber} type="num" id="seven" value="7">7</button>
-                        <button onClick={this.handleOperator} id="subtract" value="-">-</button>
-                        <button onClick={this.handleNumber} type="num" id="six" value="6">6</button>
-                        <button onClick={this.handleNumber} type="num" id="five" value="5">5</button>
-                        <button onClick={this.handleNumber} type="num" id="four" value="4">4</button>
-                        <button onClick={this.handleOperator} id="add" value="+">+</button>
-                        <button onClick={this.handleNumber} type="num" id="three" value="3">3</button>
-                        <button onClick={this.handleNumber} type="num" id="two" value="2">2</button>
-                        <button onClick={this.handleNumber} type="num" id="one" value="1">1</button>
-                        <button onClick={this.handleEquals} id="equals" value="=">=</button>
-                        <button onClick={this.handleNumber} type = "zero" id="zero" value="0">0</button>
-                        <button onClick={this.handleDecimal} id="decimal" value=".">.</button>
+                        <button className="btn" onClick={this.handleClear} id="clear" value="AC">AC</button>
+                        <button className="btn" onClick={this.handleOperator} id="divide" value="/">/</button>
+                        <button className="btn" onClick={this.handleOperator} id="multiply" value="x">*</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="nine" value="9">9</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="eight" value="8">8</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="seven" value="7">7</button>
+                        <button className="btn" onClick={this.handleOperator} id="subtract" value="-">-</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="six" value="6">6</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="five" value="5">5</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="four" value="4">4</button>
+                        <button className="btn" onClick={this.handleOperator} id="add" value="+">+</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="three" value="3">3</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="two" value="2">2</button>
+                        <button className="btn" onClick={this.handleNumber} type="num" id="one" value="1">1</button>
+                        <button className="btn" onClick={this.handleEquals} id="equals" value="=">=</button>
+                        <button className="btn" onClick={this.handleNumber} type = "zero" id="zero" value="0">0</button>
+                        <button className="btn" onClick={this.handleDecimal} id="decimal" value=".">.</button>
                     </div>
                 </div>
             </>
